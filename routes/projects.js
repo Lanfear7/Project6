9384739f2e7281358de87b684fc4ae7fd77f528c
Lanfear7 <brennan1086@gmail.com>
2020-11-19 00:17:20 -0800

    Added some new images, error handling isnt quite working

--- routes/projects.js
+++ routes/projects.js
@@ -2,10 +2,28 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data.json'); //require project data
 
+router.get('/', (req, res,) => {
+    
+    const num = req.url[1]
+    console.log(num)
+    if(isNaN(num)){
+        const err = new Error('Page not found')
+        err.status = 404
+        throw err
+    } else {
+        //this will redirect a user if the just type in projects to the first project
+        res.redirect('/projects/0')
+    }
+    
+})
+
 router.get('/:id',(req, res) => {
     const { projects } = data
-    console.log(projects[req.params.id]) //req.params.id will reference :id
-    res.render('project', {projects: projects[req.params.id]}) //projects[req.params.id] send the correct data to the pug 
+    res.render('project', {projects: projects[req.params.id]}) //projects[req.params.id] send the correct data to the pug  => req.params.id will reference :id
+    
+
+    
+
 })
 
 module.exports = router;
