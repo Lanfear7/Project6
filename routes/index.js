4ec96bd71e1a796c8642dbc88f6f9efbe06ebeda
Lanfear7 <brennan1086@gmail.com>
2020-11-29 22:44:06 -0800

    clean up

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const data = require('../data.json'); //require project data
 
 router.get('/',(req, res) => {
     res.render('index', {data: data.projects}) //render the index template from the views folder (express will look for the folder views)
-    console.log('in render')
 })
 
 module.exports = router; //makes it possible to work with the router from this file
