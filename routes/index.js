d3657276b9ca05a859c5336475187ac4b0edbdb3
Lanfear7 <brennan1086@gmail.com>
2020-11-29 22:24:34 -0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const data = require('../data.json'); //require project data
 
 router.get('/',(req, res) => {
-    res.render('index', {data: data}) //render the index template from the views folder (express will look for the folder views)
+    res.render('index', {data: data.projects}) //render the index template from the views folder (express will look for the folder views)
     console.log('in render')
 })
 
